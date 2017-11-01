ee435ec9502d3361a9ac5fd05ab9b487f24c3800
cmazzullo <chris.mazzullo@gmail.com>
2017-11-01 16:19:25 -0400

    Added info to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-automata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Playing around with cellular automata"
+  :url "http://github.com/cmazzullo/clojure-automata.git"
+  :license {:name "GNU General Public License"
+            :url "https://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clojure-automata.core
   :target-path "target/%s"
